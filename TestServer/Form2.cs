6f2746fbf1d1c0460193646db0e271d480672efb
Yurii Krutsko <yuri.krutsko@gmail.com>
2022-04-28 18:49:15 +0300

    Init TestServer

---
+++ TestServer/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestServer
+{
+    public partial class LoginForm : Form
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            Login = tbLog.Text;
+            Password = tbPass.Text;
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            tbLog.Select();
+        }
+    }
+}
---
+++ TestServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestServer
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            LoginForm loginForm = new LoginForm();
+            DialogResult dr = loginForm.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                Application.Run(new MainForm());
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
