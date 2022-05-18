5ae220e963d02924c1f37b788fc9e794a90bfdb1
Yurii Krutsko <yuri.krutsko@gmail.com>
2022-05-18 18:02:50 +0300

    Update TeestForm

--- TestClient/Form_Test.Designer.cs
+++ TestClient/Form_Test.Designer.cs
@@ -30,34 +30,35 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TestForm));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbInfo = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.lbQuestion = new System.Windows.Forms.Label();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowPanelProgress = new System.Windows.Forms.FlowLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.panel5 = new System.Windows.Forms.Panel();
             this.panel6 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
             this.panel7 = new System.Windows.Forms.Panel();
             this.panel8 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lbQuestionText = new System.Windows.Forms.Label();
+            this.lbQuestionNumber = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnFinish = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.flowLayoutPanel2.SuspendLayout();
+            this.flowPanelProgress.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lbInfo);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(1110, 100);
@@ -65,13 +66,23 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Test information";
             // 
+            // lbInfo
+            // 
+            this.lbInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbInfo.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lbInfo.Location = new System.Drawing.Point(7, 21);
+            this.lbInfo.Name = "lbInfo";
+            this.lbInfo.Size = new System.Drawing.Size(1097, 76);
+            this.lbInfo.TabIndex = 0;
+            this.lbInfo.Text = resources.GetString("lbInfo.Text");
+            // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.flowLayoutPanel2);
+            this.groupBox2.Controls.Add(this.flowPanelProgress);
             this.groupBox2.Controls.Add(this.flowLayoutPanel1);
-            this.groupBox2.Controls.Add(this.lbQuestion);
-            this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Controls.Add(this.lbQuestionText);
+            this.groupBox2.Controls.Add(this.lbQuestionNumber);
+            this.groupBox2.Controls.Add(this.pictureBox);
             this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.groupBox2.Location = new System.Drawing.Point(12, 118);
             this.groupBox2.Name = "groupBox2";
@@ -79,70 +90,20 @@
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             // 
-            // pictureBox1
+            // flowPanelProgress
             // 
-            this.pictureBox1.Image = global::TestClient.Properties.Resources.nophoto;
-            this.pictureBox1.Location = new System.Drawing.Point(6, 20);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(524, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(121, 24);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Question #25";
-            // 
-            // label2
-            // 
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(7, 21);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(1097, 76);
-            this.label2.TabIndex = 0;
-            this.label2.Text = resources.GetString("label2.Text");
-            // 
-            // lbQuestion
-            // 
-            this.lbQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lbQuestion.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.lbQuestion.Location = new System.Drawing.Point(528, 48);
-            this.lbQuestion.Name = "lbQuestion";
-            this.lbQuestion.Padding = new System.Windows.Forms.Padding(3);
-            this.lbQuestion.Size = new System.Drawing.Size(576, 110);
-            this.lbQuestion.TabIndex = 2;
-            this.lbQuestion.Text = resources.GetString("lbQuestion.Text");
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.flowLayoutPanel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(528, 161);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(10, 5, 5, 5);
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(576, 371);
-            this.flowLayoutPanel1.TabIndex = 3;
-            // 
-            // flowLayoutPanel2
-            // 
-            this.flowLayoutPanel2.Controls.Add(this.panel1);
-            this.flowLayoutPanel2.Controls.Add(this.panel2);
-            this.flowLayoutPanel2.Controls.Add(this.panel3);
-            this.flowLayoutPanel2.Controls.Add(this.panel4);
-            this.flowLayoutPanel2.Controls.Add(this.panel6);
-            this.flowLayoutPanel2.Controls.Add(this.panel5);
-            this.flowLayoutPanel2.Controls.Add(this.panel7);
-            this.flowLayoutPanel2.Controls.Add(this.panel8);
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(655, 20);
-            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(449, 25);
-            this.flowLayoutPanel2.TabIndex = 4;
+            this.flowPanelProgress.Controls.Add(this.panel1);
+            this.flowPanelProgress.Controls.Add(this.panel2);
+            this.flowPanelProgress.Controls.Add(this.panel3);
+            this.flowPanelProgress.Controls.Add(this.panel4);
+            this.flowPanelProgress.Controls.Add(this.panel6);
+            this.flowPanelProgress.Controls.Add(this.panel5);
+            this.flowPanelProgress.Controls.Add(this.panel7);
+            this.flowPanelProgress.Controls.Add(this.panel8);
+            this.flowPanelProgress.Location = new System.Drawing.Point(655, 20);
+            this.flowPanelProgress.Name = "flowPanelProgress";
+            this.flowPanelProgress.Size = new System.Drawing.Size(449, 25);
+            this.flowPanelProgress.TabIndex = 4;
             // 
             // panel1
             // 
@@ -181,15 +142,6 @@
             this.panel4.Size = new System.Drawing.Size(51, 21);
             this.panel4.TabIndex = 3;
             // 
-            // panel5
-            // 
-            this.panel5.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.panel5.Location = new System.Drawing.Point(277, 2);
-            this.panel5.Margin = new System.Windows.Forms.Padding(2);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(51, 21);
-            this.panel5.TabIndex = 4;
-            // 
             // panel6
             // 
             this.panel6.BackColor = System.Drawing.SystemColors.ControlLight;
@@ -198,6 +150,15 @@
             this.panel6.Name = "panel6";
             this.panel6.Size = new System.Drawing.Size(51, 21);
             this.panel6.TabIndex = 5;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel5.Location = new System.Drawing.Point(277, 2);
+            this.panel5.Margin = new System.Windows.Forms.Padding(2);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(51, 21);
+            this.panel5.TabIndex = 4;
             // 
             // panel7
             // 
@@ -217,6 +178,47 @@
             this.panel8.Size = new System.Drawing.Size(51, 21);
             this.panel8.TabIndex = 6;
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.flowLayoutPanel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(528, 161);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(10, 5, 5, 5);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(576, 371);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // lbQuestionText
+            // 
+            this.lbQuestionText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbQuestionText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lbQuestionText.Location = new System.Drawing.Point(528, 48);
+            this.lbQuestionText.Name = "lbQuestionText";
+            this.lbQuestionText.Padding = new System.Windows.Forms.Padding(3);
+            this.lbQuestionText.Size = new System.Drawing.Size(576, 110);
+            this.lbQuestionText.TabIndex = 2;
+            this.lbQuestionText.Text = resources.GetString("lbQuestionText.Text");
+            // 
+            // lbQuestionNumber
+            // 
+            this.lbQuestionNumber.AutoSize = true;
+            this.lbQuestionNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbQuestionNumber.Location = new System.Drawing.Point(524, 20);
+            this.lbQuestionNumber.Name = "lbQuestionNumber";
+            this.lbQuestionNumber.Size = new System.Drawing.Size(121, 24);
+            this.lbQuestionNumber.TabIndex = 1;
+            this.lbQuestionNumber.Text = "Question #25";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = global::TestClient.Properties.Resources.csharp;
+            this.pictureBox.Location = new System.Drawing.Point(6, 20);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
             // statusStrip1
             // 
             this.statusStrip1.Location = new System.Drawing.Point(0, 714);
@@ -225,32 +227,33 @@
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
-            // button3
+            // btnNext
             // 
-            this.button3.Location = new System.Drawing.Point(1026, 669);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(90, 31);
-            this.button3.TabIndex = 10;
-            this.button3.Text = "Next >>";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnNext.Location = new System.Drawing.Point(853, 669);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(90, 31);
+            this.btnNext.TabIndex = 10;
+            this.btnNext.Text = "Next >>";
+            this.btnNext.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnFinish
             // 
-            this.button2.Location = new System.Drawing.Point(930, 669);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(90, 31);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "Finish";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnFinish.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnFinish.Location = new System.Drawing.Point(1032, 669);
+            this.btnFinish.Name = "btnFinish";
+            this.btnFinish.Size = new System.Drawing.Size(90, 31);
+            this.btnFinish.TabIndex = 9;
+            this.btnFinish.Text = "Finish";
+            this.btnFinish.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnPrev
             // 
-            this.button1.Location = new System.Drawing.Point(834, 669);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 31);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "<< Previous";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnPrev.Location = new System.Drawing.Point(757, 669);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(90, 31);
+            this.btnPrev.TabIndex = 8;
+            this.btnPrev.Text = "<< Previous";
+            this.btnPrev.UseVisualStyleBackColor = true;
             // 
             // TestForm
             // 
@@ -258,9 +261,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(1134, 736);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnFinish);
+            this.Controls.Add(this.btnPrev);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -274,8 +277,8 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowPanelProgress.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -284,14 +287,14 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbInfo;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label lbQuestionNumber;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.FlowLayoutPanel flowPanelProgress;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.Label lbQuestion;
+        private System.Windows.Forms.Label lbQuestionText;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel panel4;
@@ -300,9 +303,9 @@
         private System.Windows.Forms.Panel panel7;
         private System.Windows.Forms.Panel panel8;
         private System.Windows.Forms.StatusStrip statusStrip1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnFinish;
+        private System.Windows.Forms.Button btnPrev;
     }
 }
 
--- TestClient/Properties/Resources.Designer.cs
+++ TestClient/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace TestClient.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap csharp {
+            get {
+                object obj = ResourceManager.GetObject("csharp", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap nophoto {
             get {
                 object obj = ResourceManager.GetObject("nophoto", resourceCulture);
